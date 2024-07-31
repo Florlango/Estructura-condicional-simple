460dee24c8d83d8b35fd6ba9007df5283cd0d58b
Florlango <florencialangoni@gmail.com>
2024-07-31 00:50:33 -0300

    ejercicio1.py

---
+++ ejercicio1.py
@@ -0,0 +1,8 @@
+#Realice un programa que solicite dos letras ingresadas por el usuario y verifique si son iguales o no, 
+# mostrando un mensaje.
+letra_1 =  (input ("Ingrese la primer letra: "))
+letra_2 =  (input ("Ingrese la segunda letra: "))
+if letra_1 == letra_2:
+    print ("Las letras ingresadas son iguales")
+else:
+    print ("Las letras ingresadas no son iguales")

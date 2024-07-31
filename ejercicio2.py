563536f343f1d1f9a87f56ed9622cb189d99f565
Florlango <florencialangoni@gmail.com>
2024-07-31 00:51:31 -0300

    ejercicio2.py

---
+++ ejercicio2.py
@@ -0,0 +1,7 @@
+#Hacer un programa que permita decidir si dos palabras son iguales o diferentes. Mostrar mensaje por pantalla.
+palabra_1 = (input("Ingrese la primer palabra: "))
+palabra_2 = (input("Ingrese la segunda palabra: "))
+if palabra_1 == palabra_2:
+    print ("Las palabras ingresadas" , palabra_1, "y", palabra_2, "son iguales.")
+else:
+    print ("Las palabras ingresadas" , palabra_1, "y", palabra_2, "no son iguales.")

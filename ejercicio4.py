7f34a98370b4234a30523e20ca74af7e0432d487
Florlango <florencialangoni@gmail.com>
2024-07-31 00:52:38 -0300

    ejercicio4.py

---
+++ ejercicio4.py
@@ -0,0 +1,7 @@
+#Realice un programa que lea dos números y diga cuál es el mayor.
+a = (int (5))
+b = (int(3))
+if a > b :
+    print ("El número a es mayor")
+else:
+    print ("El número b es mayor")
